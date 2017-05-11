a2730eb15440df7b465db6dbbb2af6c1563edb5e
ahadzibuli1 <ahadzibuli1@etf.unsa.ba>
2017-05-11 15:45:42 +0200

    conflicts resolved

--- REST.API/src/main/java/kjkpvik/services/ObavijestiService.java
+++ REST.API/src/main/java/kjkpvik/services/ObavijestiService.java
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-package kjkpvik.services;
 
-import kjkpvik.models.Korisnik;
-import kjkpvik.models.Obavijest;
-import kjkpvik.repositories.IKorisnikRepository;
-import kjkpvik.repositories.IObavijestiRepository;
-import kjkpvik.viewmodels.ObavijestiVM;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-/**
- * Created by Siii on 5/9/2017.
- */
-//OVO USLUZUJE KONTROLERU
-@Service
-public class ObavijestiService {
-
-
-
-}
-=======
 package kjkpvik.services;
 
 import kjkpvik.models.Korisnik;
@@ -140,4 +119,4 @@ public class ObavijestiService {
     }
 
 }
->>>>>>> efe6e0ef843f3a442e85323509617e7f4ecb8c37
+
--- REST.API/src/main/java/kjkpvik/viewmodels/ObavijestiVM.java
+++ REST.API/src/main/java/kjkpvik/viewmodels/ObavijestiVM.java
@@ -2,25 +2,14 @@ package kjkpvik.viewmodels;
 
 import java.util.Date;
 
-/**
-<<<<<<< HEAD
- * Created by Mersida on 11.5.2017.
- */
+
 public class ObavijestiVM {
 
-=======
- * Created by Kemal Halilbegović on 10.05.2017..
- */
-public class ObavijestiVM {
->>>>>>> efe6e0ef843f3a442e85323509617e7f4ecb8c37
     private String naziv;
     private String tekst;
     private Date vrijemeObjave;
     private Long korisnikID;
-<<<<<<< HEAD
 
-=======
->>>>>>> efe6e0ef843f3a442e85323509617e7f4ecb8c37
     public ObavijestiVM(){}
     public ObavijestiVM(String naziv, String tekst, Date vrijemeObjave){
         this.setNaziv(naziv);
