6ac5d63e8a5c68043f364628e41a1e7fd72fbd0d
Razija Djulovic <rdulovic1@etf.unsa.ba>
2017-05-11 14:17:18 +0200

    LokacijaServices

--- REST.API/src/main/java/kjkpvik/services/ObavijestiService.java
+++ REST.API/src/main/java/kjkpvik/services/ObavijestiService.java
@@ -1,6 +1,8 @@
 package kjkpvik.services;
 
+import kjkpvik.models.Korisnik;
 import kjkpvik.models.Obavijest;
+import kjkpvik.repositories.IKorisnikRepository;
 import kjkpvik.repositories.IObavijestiRepository;
 import kjkpvik.viewmodels.ObavijestiVM;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +15,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ObavijestiService {
 
-    @Autowired
-    private IObavijestiRepository iObavijestiRepository;
 
-    public String GetObavijesti(){ return "anisaaaaaaaa";}
-
-    public void Update(){};
-
-    public void Delete(ObavijestiVM obavijest){
-        
-
-    };
-
-    public void DodajObavijest(){};
-
-    public void FiltrirajObavijesti(){};
 
 }
