9d309d315e58fbf35551c528247aee39315af922
adnanba <ababovic1@etf.unsa.ba>
2017-05-17 21:19:23 +0200

    Implementiran navbar za pocetnu stranicu

---
+++ kjkp.client/src/components/NavbarInstance.js
@@ -0,0 +1,45 @@
+import React from "react";
+import * as ReactBootstrap from 'react-bootstrap';
+
+var rb = ReactBootstrap;
+var Navbar = rb.Navbar;
+var Nav = rb.Nav;
+var NavItem = rb.NavItem;
+var MenuItem = rb.MenuItem;
+var NavDropdown = rb.NavDropdown;
+
+const navbarInstance = (
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export class NavbarInstance extends React.Component {
+    render() {
+        return (
+          navbarInstance
+        );
+    }
+}
