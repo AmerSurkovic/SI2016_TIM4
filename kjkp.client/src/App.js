1c24fd9e5799958e2b3124d60c0b6cfab286b22d
adkl <aalibegovi2@etf.unsa.ba>
2017-05-19 00:39:08 +0200

    router configured

--- kjkp.client/src/App.js
+++ kjkp.client/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 //import * as ReactBootstrap from 'react-bootstrap';
-import {NavbarInstance} from './components/NavbarInstance';
-import {ComplaintFormInstance} from './components/ComplaintForm';
-import {ComplaintList} from './components/ComplaintList';
+import { NavbarInstance } from './components/NavbarInstance';
+import { ComplaintFormInstance } from './components/ComplaintForm';
+import { ComplaintList } from './components/ComplaintList';
 
-import {CreatingAccount} from'./components/CreatingAccount';
+import { CreatingAccount } from './components/CreatingAccount';
 
-import {AddLocationForm} from './components/AddLocationForm';
-import {DeleteLocationForm} from './components/DeleteLocationForm';
+import { AddLocationForm } from './components/AddLocationForm';
+import { DeleteLocationForm } from './components/DeleteLocationForm';
 
 
 
 class App extends React.Component {
-    render() {
-        return (
-          <div>
-            <NavbarInstance/>
+  render() {
+    return (
+      <div>
+        <NavbarInstance />
 
-            <ComplaintList/>
+        <Router>
+          <div>
+            <Route exact path="/" component={ComplaintList} />
+            <Route path="/signup" component={CreatingAccount} />
+            <Route path="/location/add" component={AddLocationForm} />
+            <Route path="/complaint" component={ComplaintFormInstance} />
+            <Route path="/location/delete" component={DeleteLocationForm} />
           </div>
-        );
-    }
+        </Router>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
