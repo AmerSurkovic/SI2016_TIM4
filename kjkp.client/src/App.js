53e29d00a1f661fa5611702415a579fe6194aeed
eavdispahi1 <eavdispahi1@hotmail.com>
2017-05-18 19:50:48 +0200

    creatingAccount

--- kjkp.client/src/App.js
+++ kjkp.client/src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import {NavbarInstance} from './components/NavbarInstance';
 import {ComplaintFormInstance} from './components/ComplaintForm';
 import {ComplaintList} from './components/ComplaintList';
-
+import {CreatingAccount} from'./components/CreatingAccount';
 
 class App extends React.Component {
     render() {
         return (
           <div>
             <NavbarInstance/>
-            <ComplaintList/>
+
+            <CreatingAccount/>
+
           </div>
         );
     }
---
+++ kjkp.client/src/components/CreatingAccount.js
@@ -0,0 +1,59 @@
+import React from "react";
+import * as ReactBootstrap from 'react-bootstrap';
+
+var rb = ReactBootstrap;
+var FormGroup = rb.FormGroup;
+var ControlLabel = rb.ControlLabel;
+var FormControl = rb.FormControl;
+var div = rb.div;
+var Button = rb.Button;
+var Form = rb.Form;
+var Col = rb.Col;
+
+const wellStyles = {maxWidth: 1000, margin: '0 auto 200px'};
+
+export class CreatingAccount extends React.Component {
+
+  render() {
+       return(
+         <div style={wellStyles} >
+          
+           <Form horizontal   >
+             <FormGroup controlId="enteringUsername">
+              <Col componentClass={ControlLabel} sm={2}  >
+                 Korisničko ime
+              </Col>
+              <Col sm={4}>
+                 <FormControl type="text" placeholder="Korisničko ime" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="enteringEmail">
+             <Col componentClass={ControlLabel} sm={2}>
+                Email
+            </Col>
+            <Col sm={4}>
+               <FormControl type="email" placeholder="Email" />
+            </Col>
+           </FormGroup>
+
+           <FormGroup controlId="formHorizontalPassword">
+             <Col componentClass={ControlLabel} sm={2}>
+                Šifra
+             </Col>
+            <Col sm={4}>
+               <FormControl type="password" placeholder="Šifra" />
+             </Col>
+           </FormGroup>
+
+           <FormGroup>
+            <Col smOffset={2} sm={10}>
+               <Button type="submit">Registruj se</Button>
+           </Col>
+          </FormGroup>
+      </Form>
+     
+    </div>
+
+);}
+}
