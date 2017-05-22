f2fb488248632b5baf3f3a259811c3423bcc71c8
AmerSurkovic <asurkovic1@etf.unsa.ba>
2017-05-22 22:48:22 +0200

    Finished authentification concernig complaints list

--- kjkp.client/src/components/complaint/ComplaintList.js
+++ kjkp.client/src/components/complaint/ComplaintList.js
@@ -3,6 +3,7 @@ import * as ReactBootstrap from 'react-bootstrap';
 import Complaint from '../complaint/Complaint';
 import { makeCancelable } from '../../globals';
 
+import AccountService from '../../services/AccountService';
 
 var rb = ReactBootstrap;
 var ListGroup = rb.ListGroup;
@@ -58,12 +59,75 @@ export class ComplaintList extends React.Component {
   }
 
   render() {
+
+    var authenticated = false;
+    var user = "";
+    var role = "";
+
+    var ActionSection;
+
     var complaints = this.state.complaints.map((comp) => (<Complaint complaint={comp} />));
     var publicComplaints = this.state.publicComplaints.map((comp) => (<Complaint complaint={comp} />));
     var privateComplaints = this.state.privateComplaints.map((comp) => (<Complaint complaint={comp} />));
     //console.log(this.state.complaints);
+
+    if(AccountService.getAuthInfo() != null){
+      var auth = AccountService.getAuthInfo();
+
+      authenticated=true;
+      user = auth.username;
+      role = auth.role;
+
+      switch (role) {
+        case "ROLE_ADMIN":
+          ActionSection =
+          <Panel>
+              <Panel header="Pregled žalbi" bsStyle="info">
+                <ListGroup>
+                  {complaints}
+                </ListGroup>
+              </Panel>
+              <Panel header="Pregled javnih žalbi" bsStyle="success">
+                <ListGroup>
+                  {publicComplaints}
+                </ListGroup>
+              </Panel>
+              <Panel header="Pregled privatnih žalbi" bsStyle="danger">
+                <ListGroup>
+                  {privateComplaints}
+                </ListGroup>
+              </Panel>
+            </Panel>
+        break;
+        case "ROLE_USER":
+          ActionSection=
+            <Panel header="Pregled javnih žalbi" bsStyle="success">
+              <ListGroup>
+                {publicComplaints}
+              </ListGroup>
+            </Panel>;
+        break;
+        case "ROLE_HR":
+        ActionSection=
+          <Panel header="Pregled javnih žalbi" bsStyle="success">
+            <ListGroup>
+              {publicComplaints}
+            </ListGroup>
+          </Panel>;
+        break;
+      }
+    }
+
     return (
       <Grid>
+        <Row className="show-grid">
+          <Col md={8} mdOffset={2}>
+          {ActionSection}
+          </Col>
+        </Row>
+      </Grid>
+    );
+    /*  <Grid>
         <Row className="show-grid">
           <Col md={8} mdOffset={2}>
 
@@ -82,11 +146,12 @@ export class ComplaintList extends React.Component {
                 {privateComplaints}
               </ListGroup>
             </Panel>
+            }
           </Col>
         </Row>
       </Grid>
 
-    );
+    );*/
 
   }
 }
