1828babca14ff5f06ea4592e13a57bec747e192f
Acimovic Aleksandar <acimovic.alek@gmail.com>
2017-05-24 15:35:22 +0200

    Complaint verification done

--- kjkp.client/src/components/complaint/ComplaintForm.js
+++ kjkp.client/src/components/complaint/ComplaintForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as ReactBootstrap from 'react-bootstrap';
+import {makeCancelable} from '../../globals';
 
 
 import ComplaintService from '../../services/ComplaintService';
@@ -19,7 +20,10 @@ var Col = rb.Col;
 export var ComplaintFormInstance = React.createClass({
 
   getInitialState: function () {
-    return { radio: 'Privatni' };
+    return { radio: 'Privatni',
+            words: [],
+            errorMessage: null
+   };
   },
 
   textChange: function (e) {
@@ -32,16 +36,64 @@ export var ComplaintFormInstance = React.createClass({
     });
   },
 
+  getWords: function() {
+    this.req = makeCancelable(fetch('http://localhost:8080/zrijeci/prikazi_rijeci'));
+    this.req.promise.then(response => response.json())
+      .then(result => this.setState({ words: result}))
+      .catch(error => this.setState({ errorMessage: error + "" }));
+  },
+
+  //Verification of words in message
+  verifyWords: function (message){
+
+    //Replace special signs and turn all characters to upper case
+    message = message.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"");
+    message = message.toUpperCase();
+
+    //Turn message in array of words
+    var msgWords = message.split(" ");
+
+    //Get forbiden words from state
+    var words = this.state.words.map((w) => (w));
+
+    // Lengths of word arrays
+    var words_len = words.length;
+    var msgWords_len = msgWords.length;
+
+    //Compare
+    for( var i = 0; i < words_len; i++ ){
+      var r = words[i].rijec.toUpperCase();
+      for(var j = 0; j < msgWords_len; j++ ){
+        if(msgWords[j] == r) return false;
+      }
+    }
+
+    return true;
+
+  },
+
   handleFormSubmit: function (formSubmitEvent) {
     formSubmitEvent.preventDefault();
     var priv = this.state.radio === 'Privatni';
 
-    ComplaintService.postComplaint(this.state.message, priv);
-    formSubmitEvent.target.reset();
-    this.setState({ radio: 'Privatni' });
+    //Is message empty?
+    if(this.state.message == null){
+      alert("Can't be empty!");
+      return;
+    }
+    
+    if(this.verifyWords(this.state.message)){
+      ComplaintService.postComplaint(this.state.message, priv);
+      formSubmitEvent.target.reset();
+      this.setState({ radio: 'Privatni' });
+    }else{
+      alert("One of the words you are using is forbidden! Pls try again!");
+    }
   },
 
   render() {
+    //Prepare forbidden words
+    this.getWords();
     return (
       <Grid>
         <Row className="show-grid">
