1269a0fe47084586181f6ec4b30eb3848da13b06
adnanba <ababovic1@etf.unsa.ba>
2017-05-26 19:13:58 +0200

    Skoro zavrsio unos anketegit add .git add .git add .git add .

--- kjkp.client/src/components/poll/AddPoll.js
+++ kjkp.client/src/components/poll/AddPoll.js
@@ -18,12 +18,73 @@ var FormControl = rb.FormControl;
 
 export class AddPoll extends React.Component {
 
+    constructor(props) {
+      super(props);
+      this.state = {
+        counter: 0,
+        questions: [],
+        text: ""
+      }
+
+      this.addQuestion = this.addQuestion.bind(this);
+      this.removeQuestion = this.removeQuestion.bind(this);
+      this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    addQuestion() {
+
+        var counter = this.state.counter;
+        counter++;
+        this.setState({ counter: counter });
+
+        var questions = this.state.questions;
+        questions.push(<PollQuestion number={counter} key={this.state.counter} onChange={this.questionChange}/>)
+        this.setState({ questions: questions });
+    }
+
+    removeQuestion() {
+      var questions = this.state.questions;
+
+      if(questions.length === 0) {
+        return;
+      }
+
+      var counter = this.state.counter;
+
+      questions.splice(counter - 1, 1);
+      counter--;
+
+      this.setState({ counter: counter });
+      this.setState({ questions: questions });
+    }
+
+    questionChange(value) {
+      //this.setState({ text: e.target.value });
+      console.log(value);
+    }
+
+    handleFormSubmit() {
+      var questions = this.state.questions;
+
+      if(questions.length === 0) {
+        alert("Anketa mora imati bar jedno pitanje")
+        return;
+      }
+
+      var counter = this.state.counter + 1;
+
+      for(var i = 0; i < counter; i++) {
+
+      }
+
+    }
+
     render() {
         const form = (
             <Grid>
               <Row className="show-grid">
                 <Col md={8} mdOffset={2}>
-                  <Panel header="Kreiranje ankete" bsStyle="success">
+                  <Panel header="Kreiranje ankete" bsStyle="info">
                     <Form horizontal>
                       <FormGroup controlId="formHorizontalName">
                         <Col md={3}>
@@ -34,12 +95,17 @@ export class AddPoll extends React.Component {
                         </Col>
                       </FormGroup>
 
-                      <Panel header="Pitanja" bsStyle="success">
-                          <Button bsStyle="info"> Dodaj pitanje </Button>
+                      <Panel header="Pitanja" bsStyle="info">
+                          <Button bsStyle="info" onClick={this.addQuestion}> Dodaj pitanje </Button>
+                          {' '}
+                          <Button bsStyle="danger" onClick={this.removeQuestion}> Obriši pitanje </Button>
 
-                          <PollQuestion/>
-
+                          <div>
+                            <br />
+                            {this.state.questions}
+                          </div>
                       </Panel>
+                      <Button bsStyle="success" onClick={this.handleFormSubmit}> Unesi anketu </Button>
                     </Form>
                   </Panel>
                 </Col>
